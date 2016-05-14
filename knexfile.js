2d84a4050097d38fe1960292cc03bd1e6072f154
Dana Silver <dsilver1221@gmail.com>
2016-05-14 15:38:40 -0400

    Add comments to knexfile.js.

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,21 @@
+// Settings Knex uses to connect to the database.
+// We use PostgreSQL in both development and production.
+// A template for a Knexfile can be generated with
+// $ knex init
+
+// See: http://knexjs.org/#knexfile
+
+// process.env.NODE_ENV || 'development'
+// is used to select the correct settings for
+// development or production.
+// See index.js for more details.
+
+// process.env.DATABASE_URL is used in production
+// on Heroku to get the database connection.
+// When we create a database, Heroku will automatically
+// set the DATABASE_URL variable. Knex knows how to read
+// this as a proper database connection.
+
 module.exports = {
   development: {
     client: 'postgresql',
